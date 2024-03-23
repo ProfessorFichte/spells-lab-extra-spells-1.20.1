e7cf6aa9a6a1f2586e134c24383a8ed5409b800f
Etienne-mc <etiennejardin@hotmail.com>
2024-03-23 18:39:37 +0100

    Initial commit

---
+++ src/main/java/net/etienne/spellslabsextra/mixin/SpellsLabExtraSpellsClient.java
@@ -0,0 +1,10 @@
+package net.etienne.spellslabsextra.mixin;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpellsLabExtraSpellsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
